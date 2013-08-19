f969363e0f923ef1534be5201b2d6c8cbc1c353d
wangqi <wangqi@wangqitekiMacBook-Air.local>
2013-08-19 11:48:56 +0800

    Initial Commit

---
+++ 1373/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1373
+//
+//  Created by wangqi on 13-8-19.
+//  Copyright (c) 2013年 wangqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
